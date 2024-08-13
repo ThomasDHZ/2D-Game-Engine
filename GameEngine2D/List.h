dc8c875b2cf66021ad35c8738f8340460998fa6f
ThomasDHZ <dothackzero@outlook.com>
2024-08-12 20:28:34 -0400

    Also got the CList working here too.

--- GameEngine2D/List.h
+++ GameEngine2D/List.h
@@ -12,48 +12,9 @@ typedef struct
 	int Capacity;
 }List;
 
-void List_StartUp(List* list, int elementSize)
-{
-	list->Data = NULL;
-	list->ElementSize = elementSize;
-	list->ItemCount = 0;
-	list->Capacity = 0;
-}
-
-void List_Resize(List* list)
-{
-	void* newPtr = malloc(list->ElementSize * list->Capacity);
-	memcpy(newPtr, list->Data, list->ElementSize * list->ItemCount);
-	
-	free(list->Data);
-	list->Data = (int*)newPtr;
-}
-
-void* List_Get(List* list, size_t index)
-{
-	char* startMemoryAddress = (char*)list->Data;
-	char* readMemoryAddress = startMemoryAddress + (index * list->ElementSize);
-	return (void*)readMemoryAddress;
-}
-
-void List_Add(List* list, void* data)
-{
-	if (list->ItemCount >= list->Capacity)
-	{
-		if (list->Capacity == 0)
-		{
-			list->Capacity = 1;
-			List_Resize(list);
-		}
-		else
-		{
-			list->Capacity *= 2;
-			List_Resize(list);
-		}
-	}
-
-	char* targetLocation = (char*)list->Data + (list->ItemCount * list->ElementSize);
-	memcpy(targetLocation, data, list->ElementSize);
-
-	list->ItemCount++;
-}
+void  List_StartUp(List* list, int elementSize);
+void  List_Resize(List* list);
+void* List_Get(List* list, size_t index);
+void  List_Add(List* list, void* data);
+void  List_Remove(List* list, int index);
+void  List_FreeMemory(List* list);
--- GameRunner/Main.cpp
+++ GameRunner/Main.cpp
@@ -4,30 +4,9 @@ extern "C"
 #include "List.h"
 }
 
-
-
 int main()
 {
-	int asdf = 324;
-	int fdas = 432;
-	List test1 = { .Data = (void*)asdf, .ElementSize = 34, .ItemCount = 23, .Capacity = 234};
-	List test2 = { .Data = (void*)fdas, .ElementSize = 64, .ItemCount = 13, .Capacity = 734 };
-
 	GameEngine_CreateGraphicsWindow("Game", 1280, 720);
-
-	List list;
-	List_StartUp(&list, sizeof(testing));
-	List_Add(&list, &test1);
-	List_Add(&list, &test2);
-	List_Add(&list, &test1);
-	List_Add(&list, &test2);
-	List_Add(&list, &test1);
-
-	List index0 = *(List*)List_Get(&list, 0);
-	List index1 = *(List*)List_Get(&list, 1);
-	List index2 = *(List*)List_Get(&list, 2);
-	List index3 = *(List*)List_Get(&list, 3);
-	List index4 = *(List*)List_Get(&list, 4);
 
 	while (global.Window.Event.type != SDL_QUIT)
 	{
